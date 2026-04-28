662ddf5631c837b46ba36a4aced4b4ab504b320e
seva-code992 <st.hajiyanni2002@gmail.com>
2026-04-28 15:03:22 +0200

    load venv
    Added SLURM job configuration for running the script.

--- scriptload.sh
+++ scriptload.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=Embeddings
+#SBATCH --output=output.txt
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
 
+source .venv/bin/activate
+uv run main.py
