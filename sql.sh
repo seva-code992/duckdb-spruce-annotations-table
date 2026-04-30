95ae90c34e5ffa361d8ac188706f7e71fb37dee2
Seva <st.hajiyanni2002@gmail.com>
2026-04-30 10:32:52 +0200

    Duckdb test.db database for slurm

---
+++ sql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=DuckDB
+#SBATCH --output=output_ii.txt
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+
+duckdb test.db
+duckdb test.db -f spruce.sql
+duckdb test.db -f cosine-similarity.sql 
