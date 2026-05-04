c110c386c4a360b4199f568c1016211e7c65716c
seva-code992 <st.hajiyanni2002@gmail.com>
2026-05-04 13:16:09 +0200

    Increase computing power
    Updated resource allocation and added GPU support.

--- sql.sh
+++ sql.sh
@@ -3,8 +3,12 @@
 #SBATCH --output=output_ii.txt
 #SBATCH --time=10:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4      #Opt: Up to 20
+#SBATCH --mem=16G          #Opt: Up to 32G
+#SBATCH --gres=gpu:tesla:1  #Opt: I could request both Tesla GPUs --gres=gpu:tesla:2  
+#SBATCH --partition=gpu
+
+
 
 duckdb test.db
 duckdb test.db -f spruce.sql
